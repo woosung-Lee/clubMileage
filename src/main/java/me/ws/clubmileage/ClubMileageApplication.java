6f23e445e1b0104ef9552b6ce7d5dd05129ad405
woosung-Lee <woosung.lee86@gmail.com>
2022-06-18 22:33:48 +0900

    Initial commit

---
+++ src/main/java/me/ws/clubmileage/ClubMileageApplication.java
@@ -0,0 +1,13 @@
+package me.ws.clubmileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubMileageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubMileageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ws/clubmileage/ClubMileageApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ws.clubmileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubMileageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
